f9a762364a9c66bf457d5f9bb4005a43327c2f61
petekgithub <petekshamitbeyli@gmail.com>
2021-02-06 22:40:13 +0300

    sending data to server

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const server = http.createServer((req, res) => {
         'Content-Type':'application/json',
         'X-Powered-By': 'Node.js'
     });
+
+    let body = [];
+
+    req.on('data', chunk => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log(body); 
+    })
+
    
     res.end(JSON.stringify({
         success: false,
